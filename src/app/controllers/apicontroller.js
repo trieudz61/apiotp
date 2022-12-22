a7528444b35e7968b0d091f38091f551bbfef86b
trieudz61 <94054180+trieudz61@users.noreply.github.com>
2022-12-22 22:18:30 +0700

    update

--- src/app/controllers/apicontroller.js
+++ src/app/controllers/apicontroller.js
@@ -1,20 +1,112 @@
 const axios = require("axios");
 
 class apiController {
-  getsim = async (req, res, next) => {
-    console.log(req.query);
-    // const callbackData = await getNewSim();
-    // console.log(callbackData);
-    res.send(req.query);
+  getSim = async (req, res) => {
+    const dataAPIQuery = req.query.api;
+    const dataIdQuery = req.query.id;
+    const callbackData = await getNewSim(dataAPIQuery, dataIdQuery);
+    res.send(callbackData);
+  };
+  getOTP = async (req, res) => {
+    const dataAPIQuery = req.query.api;
+    const dataRequesyIdQuery = req.query.request_id;
+    const callbackData = await getOTPFromId(dataAPIQuery, dataRequesyIdQuery);
+    res.send(callbackData);
+  };
+  getServices = async (req, res) => {
+    const dataAPIQuery = req.query.api;
+    const callbackData = await servicesAvailable(dataAPIQuery);
+    console.log(callbackData.message);
+    if (callbackData.message === "Invalid api key!") {
+      res.send(callbackData);
+    } else {
+      const trueData = [
+        {
+          id: "1",
+          name: "facebook",
+          price: "1200",
+          timeout: "300",
+        },
+        {
+          id: "2",
+          name: "hotmail",
+          price: "800",
+          timeout: "300",
+        },
+        {
+          id: "3",
+          name: "tiktok",
+          price: "1000",
+          timeout: "300",
+        },
+        {
+          id: "4",
+          name: "zalo",
+          price: "2000",
+          timeout: "300",
+        },
+        {
+          id: "5",
+          name: "bigo",
+          price: "800",
+          timeout: "300",
+        },
+        {
+          id: "6",
+          name: "facebook2",
+          price: "1200",
+          timeout: "300",
+        },
+      ];
+      res.send(trueData);
+    }
   };
 }
-
-let getNewSim = async (query1,query2) => {
+let servicesAvailable = async (query1) => {
   return new Promise(async (resolve, reject) => {
     try {
       await axios({
         method: "get",
-        url: "https://api.viotp.com/request/getv2?token=f247344c40df47ceb7ecf9371b32c860&serviceId=45&network=",
+        url:
+          "http://otptextnow.com/api/?key=" +
+          query1 +
+          "&action=get_all_services",
+      }).then((res) => {
+        resolve(res.data);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+let getOTPFromId = async (query1, query2) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await axios({
+        method: "get",
+        url:
+          "http://otptextnow.com/api/?key=" +
+          query1 +
+          "&action=get_code&id=" +
+          query2,
+      }).then((res) => {
+        resolve(res.data);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+let getNewSim = async (query1, query2) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await axios({
+        method: "get",
+        url:
+          "http://otptextnow.com/api/?key=" +
+          query1 +
+          "&action=get_number&id=" +
+          query2,
       }).then((res) => {
         resolve(res.data);
       });
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const apiController = require("../app/controllers/apicontroller");
-router.get("/api/getsim", apiController.getsim);
+router.get("/getsim", apiController.getSim);
+router.get("/getotp", apiController.getOTP);
+router.get("/getallservices", apiController.getServices);
 module.exports = router;
